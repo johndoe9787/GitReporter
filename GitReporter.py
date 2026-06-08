1a41fde6ccc8c5126f6c471bc297051567b7882f
johndoe9787 <9787johndoe@gmail.com>
2026-06-08 04:34:30 -0500

    Add GitReporter.py fixed crash for none values

--- GitReporter.py
+++ GitReporter.py
@@ -1,6 +1,6 @@
 import requests
 
-class reporter:
+class Reporter:
     
     def __init__(self):
         self.repos = []
@@ -11,29 +11,25 @@ class reporter:
             username = self.get_input()
             url = "https://api.github.com/users/" + username + "/repos"
             response = requests.get(url)
-            self.repos = response.json()
-            status = response.status_code
-            print(url)
-            print(response.status_code)
+            status = response.status_code          
             
             if status == 200:
+                self.repos = response.json()
                 self.display_data(self.repos)
                 break
             else:
                 print("Invalid Github username")
                 continue
 
-
-
     def get_input(self):
         answer = input("Enter Github username: ")
         return answer
     
-    def display_data(self,lists):
-        for list in lists:
-            print(list["name"] + ", " + list["language"] + ", " + str(list["stargazers_count"]))
+    def display_data(self,repos):
+        for repo in repos:
+            print(f"{repo["name"] or "empty"}, {repo["language"] or "empty"}, {repo["stargazers_count"]} ")
 
 
 if __name__ == "__main__":
-    Githubrepo = reporter()
+    Githubrepo = Reporter()
     Githubrepo.run()
